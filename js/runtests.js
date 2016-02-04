764b434ee9bc467c882986adf790b2b5c9adaefe
Ioannis Charalampidis <ioannis.charalampidis@cern.ch>
2016-02-04 22:17:11 +0100

    Adding some UI to the tests

--- js/runtests.js
+++ js/runtests.js
@@ -8,10 +8,13 @@ var profileLoader = require("jbb-profile-three/loader");
 var BundlesLoader = require("jbb/loader");
 var BinaryDecoder = require("jbb/decoder");
 
+// Get jquery for UI stuff
+var $ = require('jquery');
+
 /**
  * Run test by trying to load the source and the binary
  */
-function run_test( bundle ) {
+function run_test( bundle, row, callback ) {
 
 	// Instantiate a new bundles loader
 	var sourceLoader = new BundlesLoader( profileLoader, 'bundles' );
@@ -50,17 +53,62 @@ function run_test( bundle ) {
 		});
 	}
 
+	// Reset rows
+	row.find(".v-source").text("Running...");
+	row.find(".v-compact").text("Running...");
+	row.find(".v-sparse").text("Running...");
+
 	// Run binary first
-	load_sparse_binary(function() {
+	var loadStart = Date.now();
+	load_source(function() {
+		var loadDelta = Date.now() - loadStart;
+		row.find(".v-source").text( loadDelta + " ms");
+
 		// Wait a sec
 		setTimeout(function() {
 			// Load source
-			load_source(function() {
-				console.log("-- test completed --");
+			var loadStart = Date.now();
+			load_binary(function() {
+				loadDelta = Date.now() - loadStart;
+				row.find(".v-compact").text( loadDelta + " ms");
+
+				// Wait a sec
+				setTimeout(function() {
+					// Load source
+					var loadStart = Date.now();
+					load_sparse_binary(function() {
+						loadDelta = Date.now() - loadStart;
+						row.find(".v-sparse").text( loadDelta + " ms");
+
+						// Trigger callback
+						callback();
+
+					});
+				}, 1000);
+
 			});
 		}, 1000);
+
 	});
 
 }
+
+// Bind on click
+$(function() {
+
+	$("button#start-tests").click(function() {
+
+		// Chain-run all tests
+		run_test("animated", $("#times-animated"), function() {
+			run_test("heavy", $("#times-heavy"), function() {
+				run_test("md2", $("#times-md2"), function() {
+
+				});
+			});
+		});
+
+	});
+
+});
 
 global.run_test = run_test;
