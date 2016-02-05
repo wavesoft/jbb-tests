21a9e1e747595d65289ca1f0e46bb1c64918c9b1
Ioannis Charalampidis <icharala@macaliceichar.cern.ch>
2016-02-05 14:23:57 +0100

    Updating tests to use the dist version of scripts

--- gulpfile.js
+++ gulpfile.js
@@ -35,58 +35,6 @@ gulp.task('bundles.sparse', function() {
 		.pipe(gulp.dest('build/bundles'));
 
 });
-gulp.task('bundles', ['bundles.compact', 'bundles.sparse']);
 
-//
-// Compile the sources 
-//
-gulp.task('script-timing', function() {
-	return gulp.src('js/test-timing.js')
-		.pipe(webpack({
-			module: {
-				loaders: [
-					{ test: /\.json$/, loader: 'json' },
-				],
-		    },
-		    node: {
-		    	'fs': 'empty'
-		    },
-		    output: {
-		    	'filename': 'test-timing.js'
-		    },
-		    plugins: [
-		    	new webpack.webpack.optimize.DedupePlugin()
-		    ]
-		}))
-		.pipe(uglify("test-timing.min.js", { outSourceMap: true }))
-		.pipe(gulp.dest('build'));
-});
-
-//
-// Compile the sources 
-//
-gulp.task('script-loader', function() {
-	return gulp.src('js/test-loader.js')
-		.pipe(webpack({
-			module: {
-				loaders: [
-					{ test: /\.json$/, loader: 'json' },
-				],
-		    },
-		    node: {
-		    	'fs': 'empty'
-		    },
-		    output: {
-		    	'filename': 'test-loader.min.js'
-		    },
-		    plugins: [
-		    	new webpack.webpack.optimize.DedupePlugin()
-		    ]
-		}))
-		// .pipe(uglify("test-loader.min.js", { outSourceMap: true }))
-		.pipe(gulp.dest('build'));
-});
-
-// By default run only script
-gulp.task('default', ['script-timing']);
-
+// By default build bundles
+gulp.task('default', ['bundles.compact', 'bundles.sparse']);
